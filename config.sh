1e0a87b3baef25f3f31f443bd2b754930ecfd3f1
YanivYuzis <YanivYuzis@users.noreply.github.com>
2021-12-16 10:00:27 +0200

    aa

--- config.sh
+++ config.sh
@@ -1,4 +1,8 @@
-echo InScript > ./test.txt
-#apt-get install -y nginx
-wget https://raw.githubusercontent.com/YanivYuzis/fred/main/ibm.html -O /var/www/html/index.nginx-debian.html
-systemctl start nginx
+SOURCE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cp ${SOURCE_PATH}/2_l.jpg /var/www/html/2_l.jpg
+#cp /xxx/yyy/zzz.pdf /var/www/html/file.pdf
+
+nohup busybox httpd -h ${SOURCE_PATH}/ -p 8080 > ${SOURCE_PATH}/httpd.out 2>&1 &
+
+nohup node ${SOURCE_PATH}/load.js > ${SOURCE_PATH}/node.out 2>&1 &
