8fbef0c9939cf99df32f917f0fd5bbb1f382bb73
YanivYuzis <YanivYuzis@users.noreply.github.com>
2021-12-16 10:05:44 +0200

    111

--- config.sh
+++ config.sh
@@ -3,9 +3,10 @@ set -e
 
 SOURCE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cp ${SOURCE_PATH}/index.html /var/www/html/index.nginx-debian1.html
 #cp ${SOURCE_PATH}/2_l.jpg /var/www/html/2_l.jpg
 #cp /xxx/yyy/zzz.pdf /var/www/html/file.pdf
 
-nohup busybox httpd -h ${SOURCE_PATH}/ -p 8080 > ${SOURCE_PATH}/httpd.out 2>&1 &
+nohup busybox httpd -h ${SOURCE_PATH}/ -p 0.0.0.0:8082 > ${SOURCE_PATH}/httpd.out 2>&1 &
 
 nohup node ${SOURCE_PATH}/load.js > ${SOURCE_PATH}/node.out 2>&1 &
