7886377cca33f9a0eb36d204baf3bc196f140f78
YanivYuzis <YanivYuzis@users.noreply.github.com>
2021-12-16 10:07:51 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,8 +3,8 @@ set -e
 
 SOURCE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#cp ${SOURCE_PATH}/index.html /var/www/html/index.nginx-debian1.html
-#cp ${SOURCE_PATH}/2_l.jpg /var/www/html/2_l.jpg
+cp ${SOURCE_PATH}/index.html /var/www/html/index.nginx-debian1.html
+cp ${SOURCE_PATH}/2_l.jpg /var/www/html/2_l.jpg
 #cp /xxx/yyy/zzz.pdf /var/www/html/file.pdf
 
 nohup busybox httpd -h ${SOURCE_PATH}/ -p 0.0.0.0:8082 > ${SOURCE_PATH}/httpd.out 2>&1 &
